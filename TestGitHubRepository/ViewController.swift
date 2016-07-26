66e9d326b0bd8b20efbb8e6b7b5019f37b26bdce
Remote Admin <rdadmin@I46221P65081523.local>
2016-07-26 14:07:51 -0500

    Initial Commit

---
+++ TestGitHubRepository/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGitHubRepository
+//
+//  Created by Remote Admin on 7/26/16.
+//  Copyright (c) 2016 Jennifer Roscoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
